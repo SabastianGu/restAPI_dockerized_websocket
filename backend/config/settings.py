e02ac91c2515a4d4e414e5d4c214a0a6467c7480
nick <nickvoevodin@icloud.com>
2023-01-23 20:31:26 +0300

    Обновление сеттинг

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,20 +64,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT')
     }
-}
-
-    #'default': {
-    #    'ENGINE': 'django.db.backends.postgresql',
-    #    'NAME': os.getenv('NAME'),
-    #    'USER': os.getenv('USER'),
-    #    'PASSWORD': os.getenv('PASSWORD'),
-    #    'HOST': os.getenv('HOST'),
-    #    'PORT': os.getenv('PORT')
-    #}
 }
 
 # Password validation
