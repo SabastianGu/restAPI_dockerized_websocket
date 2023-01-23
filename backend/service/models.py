34f0543482803e289361e9ffccb4d822ffbd0811
nick <nickvoevodin@icloud.com>
2023-01-23 20:30:47 +0300

    Обновление WorkingMode

--- backend/service/models.py
+++ backend/service/models.py
@@ -47,10 +47,10 @@ class WorkingMode(models.Model):
         ('closed', 'closed'),
     ]
 
-    time_lte_hour = models.IntegerField  # <
-    time_gte_hour = models.IntegerField  # > от
-    time_lte_min = models.IntegerField  # < до
-    time_gte_min = models.IntegerField  # >
+    time_lte_hour = models.IntegerField()  # <
+    time_gte_hour = models.IntegerField()  # > от
+    time_lte_min = models.IntegerField()  # < до
+    time_gte_min = models.IntegerField()  # >
     pass_mode = models.CharField(max_length=999, choices=PASSMODE_CHOICES)  # pay_by_hour | pay_by_interval | closed
     free_time_min = models.IntegerField(default=0)
     interval = models.IntegerField(default=0)
@@ -58,7 +58,10 @@ class WorkingMode(models.Model):
     price = models.IntegerField(default=0)
     entry_fee = models.IntegerField(default=0)
     description = models.CharField(max_length=999)
-    access_points = models.ManyToManyField('AccessPoint')
+
+    def __str__(self):
+        return self.description
+
 
 class AccessPoint(models.Model):
 
@@ -70,9 +73,9 @@ class AccessPoint(models.Model):
     cam_id = models.ForeignKey('Camera', on_delete=models.PROTECT)
     laurent2_id = models.ForeignKey('Laurent2', on_delete=models.PROTECT)
     led_board_id = models.ForeignKey('LEDBoard', on_delete=models.PROTECT)
-    relay_number = models.IntegerField
-    out_number_magnet_loop_before = models.IntegerField
-    out_number_magnet_loop_after = models.IntegerField
+    relay_number = models.IntegerField()
+    out_number_magnet_loop_before = models.IntegerField()
+    out_number_magnet_loop_after = models.IntegerField()
     working_modes = models.ManyToManyField('WorkingMode')
 
     direction = models.CharField(max_length=3, choices=EXIT_CHOICES, db_index=True)  # in, out
